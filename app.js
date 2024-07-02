2715a2a1ccbd3730e1de5474b3ee2a6ca78578b2
Scott Johnstone <scottajohnstone@gmail.com>
2024-07-02 16:31:22 -0500

    Created loginRequired middleware

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(session({ secret: 'ssshhhhh', saveUninitialized: true, resave: false }));
 
+function loginRequired(req, res, next) {
+    if (!req.session.userId) {
+      res.status(401).json({ error: 'Unauthorized' });
+    } else {
+      next()
+    }
+  }
+
 app.get('/api/movies', async (req, res) => {
     const allMovies = await Movie.findAll();
     res.json(allMovies)
@@ -36,13 +44,34 @@ app.post('/api/auth', async (req, res) => {
     }
 });
 
-app.post('/api/logout', (req, res) => {
-    if (!req.session.userId){
-        res.status(401).json({ error: 'Unauthorized' });
-    }   else {
-        req.session.destroy();
-        res.json({ success: true })
+app.post('/api/logout', loginRequired, (req, res) => {
+    req.session.destroy();
+    res.json({ success: true })
     }
-});
+);
+
+app.get('/api/ratings', loginRequired, async (req, res) => {
+    const { userId } = req.session;
+
+    const user = await User.findByPk(userId);
+    const ratings = await user.getRatings({
+    include: {
+        model: Movie,
+        attributes: ['title'],
+    },
+    });
+    res.json(ratings)
+    });
+
+app.post('/api/ratings', loginRequired, async (req, res) => {
+    const { userId } = req.session;
+    
+      const { movieId, score } = req.body;
+  
+      const user = await User.findByPk(userId);
+      const rating = await user.createRating({ movieId: movieId, score: score });
+      res.json(rating);
+    }
+  );
 
 ViteExpress.listen(app, port, () => console.log(`Server is listening on http://localhost:${port}`));
