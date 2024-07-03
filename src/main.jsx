3d4f98e5c521ad8583fbb768c263e895e54d4530
Scott Johnstone <scottajohnstone@gmail.com>
2024-07-03 13:57:08 -0500

    added data routers/loader functions

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
@@ -10,11 +11,32 @@ import App from './App.jsx';
 import './css/index.css';
 import ErrorPage from './pages/ErrorPage.jsx';
 import IndexPage from './pages/IndexPage.jsx';
+import AllMoviesPage from './pages/AllMoviesPage.jsx';
+import MovieDetailPage from './pages/MovieDetailPage.jsx';
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />} errorElement={<ErrorPage />}>
       <Route index element={<IndexPage />} />
+
+      <Route 
+      path='movies' 
+      element={<AllMoviesPage/>} 
+      loader={async () => {
+        const res = await axios.get('/api/movies')
+        return {movies: res.data}
+      }} 
+      />
+
+      <Route 
+      path='movies/:movieId' 
+      element={<MovieDetailPage/>} 
+      loader={async ({params}) => {
+        const res = await axios.get(`/api/movies/${params.movieId}`)
+        return {movie: res.data}
+      }} 
+      />
     </Route>,
   ),
 );
--- src/pages/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage.jsx
@@ -1,13 +1,31 @@
 import { useLoaderData } from 'react-router-dom';
 
 export default function MovieDetailPage() {
-  const response = useLoaderData();
-  const movieDetail = response.data;
+  const {movie: {title, posterPath, overview, releaseDate}} = useLoaderData();
+
+  const months = {
+    '01': 'January',
+    '02': 'February',
+    '03': 'March',
+    '04': 'April',
+    '05': 'May',
+    '06': 'June',
+    '07': 'July',
+    '08': 'August',
+    '09': 'September',
+    '10': 'October',
+    '11': 'November',
+    '12': 'December'
+  }
+  
+  const prettyDate = releaseDate.split('').slice(0, 10).join('').split('-')
 
   return (
     <>
-      <h1>Movie Detail Page</h1>
-      <p>TODO</p>
+      <h1>{title}</h1>
+      <h2>{months[prettyDate[1]]} {prettyDate[2]}, {prettyDate[0]}</h2>
+      <img src={posterPath} alt={title} style={{width:'200px'}} />
+      <p>{overview}</p>
     </>
   );
 }
